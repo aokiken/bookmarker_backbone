16e54f5176a2e52eb6b89a81e218b1fa94a04e2e
ken aoki <aokiken@me.com>
2016-08-25 09:06:41 +0900

    initial commit

--- src/javascripts/models/bookmarks_tags.js
+++
@@ -1,9 +0,0 @@
-app.models.BookmarksTags = Backbone.Model.extend({
-  defaults: {
-    bookmark_id: '',
-    tag_id: ''
-  },
-  initialize: function () {
-    return this;
-  }
-});
--- src/javascripts/vendors/backbone.localStorage.js
+++
@@ -1,1 +0,0 @@
-../../../node_modules/backbone.localstorage/backbone.localStorage.js
